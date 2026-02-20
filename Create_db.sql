a26e7502fe4f3aae803081e593ebd9f3650ac15f
bananateo <teodorkamchev@gmail.com>
2026-02-20 16:07:29 +0200

    Upgraded DB initialization
    The database is now created with all the needed tables and their connections.

--- Create_db.sql
+++ Create_db.sql
@@ -1,23 +1,32 @@
 create database kanban;
 
 CREATE TABLE tasks (
-  Posittion INT,
-  Token INT,
-  deleted BOOL,
+  Position INT not null,
+  Token INT not null,
+  Deleted BOOL not null,
   Name VARCHAR(255),
   Description VARCHAR(255),
-  priority INT,
-  state INT,
-  time timestamp
+  Priority INT not null,
+  State INT not null,
+  Time timestamp not null,
+  
+  FOREIGN KEY (Token) REFERENCES users(Token),
+  FOREIGN KEY (State) REFERENCES states(id)
 );
 
 CREATE TABLE users (
-  Token INT,
+  Token INT primary key,
   Username VARCHAR(255)
 );
 
 CREATE TABLE states (
-  Newly_created INT default 0,
-  Work_in_progress INT default 1,
-  Finished INT default 2
+  Id int primary key,
+  Name VARCHAR(32)
 );
+
+INSERT INTO states (id, name) VALUES
+(0, 'Newly created'),
+(1, 'Work in progress'),
+(2, 'Finished');
+
+
